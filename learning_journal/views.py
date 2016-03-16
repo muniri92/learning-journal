820bdd89c6e0783812a46c213f71ef5c83f40d4e
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-03-16 16:35:45 -0700

    beginning view setup

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,4 +1,3 @@
-from pyramid.response import Response
 from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
@@ -9,28 +8,12 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(Entry).filter(Entry.title == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'learning_journal'}
+# templates/base.jinja2
+@view_config(route_name='home', renderer='string')
+def list_view(request):
+    return 'This is the list view'
 
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_learning_journal_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
-
+@view_config(route_name='entry', renderer='string')
+def detail_view(request):
+    return 'You selected entry number: {entry}'.format(**request.matchdict)
